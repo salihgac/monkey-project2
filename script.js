ea77505fedda78a66b7af189a84a57d101d89b24
salihgac <salih_agac@hotmail.com>
2025-03-26 15:13:47 +0300

    done

--- script.js
+++ script.js
@@ -30,22 +30,22 @@ document.addEventListener("DOMContentLoaded", function () {
         let monkeyType, monkeyImage;
         if (score.thoughtful >= score.simple && score.thoughtful >= score.shocked && score.thoughtful >= score.cool && score.thoughtful >= score.sad && score.thoughtful >= score.fancy) {
             monkeyType = "overthink maymun 🐵";
-            monkeyImage = "images/düşünceli.jpeg"; // Ensure this file exists
+            monkeyImage = "./images/düşünceli.jpeg"; // Ensure this file exists
         } else if (score.simple >= score.thoughtful && score.simple >= score.shocked && score.simple >= score.cool && score.simple >= score.sad && score.simple >= score.fancy) {
             monkeyType = "düz maymun 🐒";
-            monkeyImage = "images/düz.jpg"; // Ensure this file exists
+            monkeyImage = "./images/düz.jpg"; // Ensure this file exists
         } else if (score.shocked >= score.thoughtful && score.shocked >= score.simple && score.shocked >= score.cool && score.shocked >= score.sad && score.shocked >= score.fancy) {
             monkeyType = "şaşkın maymıun 🐵";
-            monkeyImage = "images/şaşkın.jpg"; // Ensure this file exists
+            monkeyImage = "./images/şaşkın.jpg"; // Ensure this file exists
         } else if (score.cool >= score.thoughtful && score.cool >= score.simple && score.cool >= score.shocked && score.cool >= score.sad && score.cool >= score.fancy) {
             monkeyType = "havali maymun 🐒";
-            monkeyImage = "images/havalı.gif"; // Ensure this file exists
+            monkeyImage = "./images/havalı.gif"; // Ensure this file exists
         } else if (score.sad >= score.thoughtful && score.sad >= score.simple && score.sad >= score.shocked && score.sad >= score.cool && score.sad >= score.fancy) {
-            monkeyType = "üzgün maymun 🐵";
+            monkeyType = "./üzgün maymun 🐵";
             monkeyImage = "images/sad-monkey.gif"; // Ensure this file exists
         } else if (score.fancy >= score.thoughtful && score.fancy >= score.simple && score.fancy >= score.shocked && score.fancy >= score.cool && score.fancy >= score.sad) {
             monkeyType = "süslü maymun 🐒";
-            monkeyImage = "images/süslü.gif"; // Ensure this file exists
+            monkeyImage = "./images/süslü.gif"; // Ensure this file exists
         }
 
         localStorage.setItem("monkeyResult", monkeyType);
